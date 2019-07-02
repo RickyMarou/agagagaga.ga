ea00112e88dd36fead66b2c120d749eae5d8de75
Marwan Zibaoui <marwan@wunderflats.com>
2019-07-02 23:31:34 +0200

    keep it funny

--- layers.js
+++ layers.js
@@ -14,6 +14,22 @@ window.addEventListener("scroll", () => {
   scrollPosition = window.scrollY
 })
 
+document.addEventListener("click", mouseMoveEvent => {
+  mousePosition = {
+    x: mouseMoveEvent.clientX,
+    y: mouseMoveEvent.clientY + scrollPosition
+  }
+  const newLayer = layer.cloneNode(true)
+  newLayer.setAttribute("data-layer", layerCount++)
+  newLayer.addEventListener("animationend", function animationend() {
+    newLayer.removeEventListener("animationend", animationend)
+    main.removeChild(newLayer)
+  })
+  main.appendChild(newLayer)
+  newLayer.style.setProperty("--circleX", `${mousePosition.x}px`)
+  newLayer.style.setProperty("--circleY", `${mousePosition.y}px`)
+})
+
 setInterval(() => {
   const newLayer = layer.cloneNode(true)
   newLayer.setAttribute("data-layer", layerCount++)
@@ -32,4 +48,4 @@ setInterval(() => {
   })
   newLayer.style.setProperty("--circleX", `${x}px`)
   newLayer.style.setProperty("--circleY", `${y}px`)
-}, 5000)
+}, 3000)
